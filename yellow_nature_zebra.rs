6085c6922897075348bfda99e1aeb5b8e20f0bf7
algonzales95 <136688650+algonzales95@users.noreply.github.com>
2023-06-26 21:52:44 +0700

    Fix issue reported by user

---
+++ yellow_nature_zebra.rs
@@ -0,0 +1,69 @@
+fn main() {
+    println!("Welcome to The Green Initiative!");
+ 
+    // 1. Reuse, Reduce, and Recycle
+    let reuse = "Use reusable bags when you go shopping for groceries or other items.";
+    let reduce = "Avoid buying single-use items, like plastic water bottles.";
+    let recycle = "Recycle glass, plastic, and paper waste properly.";
+ 
+    println!("We can help the environment by practicing the 3 Rs - Reuse, Reduce, and Recycle!");
+    println!("{}", reuse);
+    println!("{}", reduce);
+    println!("{}", recycle);
+ 
+    // 2. Conserve Water
+    let conserve_water = "Turn off the tap when brushing your teeth or washing dishes.";
+ 
+    println!("We can also help by conserving water!");
+    println!("{}", conserve_water);
+ 
+    // 3. Travel Sustainably
+    let travel_sustainably = "Choose to walk, bike, or take public transportation whenever possible.";
+ 
+    println!("Let's also travel sustainably!");
+    println!("{}", travel_sustainably);
+ 
+    // 4. Shop Second Hand
+    let second_hand = "Look for second-hand items before buying new ones.";
+ 
+    println!("Another way to help the environment is by shopping second hand!");
+    println!("{}", second_hand);
+ 
+    // 5. Buy Local & Seasonal
+    let local_seasonal = "Try to buy locally grown and seasonal food when possible.";
+ 
+    println!("Let's also buy local and seasonal food!");
+    println!("{}", local_seasonal);
+ 
+    // 6. Compost
+    let compost = "Compost food scraps and yard waste.";
+ 
+    println!("Composting is also a great way to reduce waste and help the environment!");
+    println!("{}", compost);
+ 
+    // 7. Unplug Electronics 
+    let unplug = "Turn off electronics and unplug them when not in use.";
+ 
+    println!("We can also help by unplugging any electronics when not in use!");
+    println!("{}", unplug);
+ 
+    // 8. Reduce Energy Usage
+    let energy = "Turn off lights and unplug appliances that are not in use.";
+ 
+    println!("And for energy savings, we should turn off lights and unplug appliances that are not in use!");
+    println!("{}", energy);
+ 
+    // 9. Use Enviornmentally-Friendly Cleaning Products
+    let clean = "Look for organic and natural cleaning products with minimal packaging.";
+ 
+    println!("We can also help by using enviornmentally-friendly cleaning products!");
+    println!("{}", clean);
+ 
+    // 10. Plant Native Species 
+    let native = "Plant native species of plants in your garden.";
+ 
+    println!("Finally, let's all try to plant native species of plants in our gardens!");
+    println!("{}", native);
+ 
+    println!("Thank you for participating in The Green Initiative!");
+}
