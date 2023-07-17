173d1602688985fd352625b9e5018706bee82700
algonzales95 <136688650+algonzales95@users.noreply.github.com>
2023-07-17 19:45:11 +0700

    Add support for multiple languages

---
+++ tree_house_zebra.js
@@ -0,0 +1,379 @@
+//The Green Initiative:
+//This is a simple initiative to reduce our carbon footprint by using less energy and saving more resources.
+
+//1. Change to energy-efficient light bulbs: 
+var bulbs = document.querySelectorAll("lightbulb");
+for(var i = 0; i < bulbs.length; i++) {
+ bulbs[i].src = "greenbulb.png";
+} 
+
+//2. Install solar panels: 
+function installSolarPanels() {
+ var solarPanels = document.querySelectorAll("Solarpanel");
+ for(var i = 0; i < solarPanels.length; i++) {
+  solarPanels[i].src = "greensolarpanel.png";
+ }
+}
+
+//3. Replace appliances with energy-star rated ones:
+function replaceAppliances(){
+ var applianceList = document.querySelectorAll("appliance");
+ for(var i = 0; i < applianceList.length; i++) {
+  applianceList[i].src = "greenappliance.png";
+ }
+}
+
+//4. Unplug electrical appliances when not in use:  
+function unplugAppliances(){
+ var unplugList = document.querySelectorAll("unpluggedappliance");
+ for(var i = 0; i < unplugList.length; i++) {
+  unplugList[i].src = "unpluggedappliance.png";
+ }
+}
+
+//5. Use natural cleaners and detergents:
+function useNaturalCleaners(){
+ var naturalList = document.querySelectorAll("naturalcleaner");
+ for(var i = 0; i < naturalList.length; i++) {
+  naturalList[i].src = "greennaturalcleaner.png";
+ }
+}
+
+//6. Use reusable bags:
+function useReusableBags(){
+ var reusableList = document.querySelectorAll("reusablebag");
+ for(var i = 0; i < reusableList.length; i++) {
+  reusableList[i].src = "greenreusablebag.png";
+ }
+}
+
+//7. Reduce water consumption:
+function reduceWaste(){
+ var wasteList = document.querySelectorAll("wastecontrol");
+ for(var i = 0; i < wasteList.length; i++) {
+  wasteList[i].src = "greenwastecontrol.png";
+ }
+}
+
+//8. Plant trees:
+function plantTrees(){
+ var treesList = document.querySelectorAll("tree");
+ for(var i = 0; i < treesList.length; i++) {
+  treesList[i].src = "greentree.png";
+ }
+}
+
+//9. Use public transportation:
+function usePublicTransport(){
+ var transportList = document.querySelectorAll("publictransport");
+ for(var i = 0; i < transportList.length; i++) {
+  transportList[i].src = "greenpublictransport.png";
+ }
+}
+
+//10. Don’t idle your car:
+function stopIdlingCar(){
+ var idleList = document.querySelectorAll("idlecar");
+ for(var i = 0; i < idleList.length; i++) {
+  idleList[i].src = "greenidlecar.png";
+ }
+}
+
+//11. Turn off lights when not in use:
+function turnOffLights(){
+ var lightsList = document.querySelectorAll("light");
+ for(var i = 0; i < lightsList.length; i++) {
+  lightsList[i].src = "greenlight.png";
+ }
+}
+
+//12. Limit red meat consumption:
+function limitRedMeatConsumption(){
+ var meatList = document.querySelectorAll("meat");
+ for(var i = 0; i < meatList.length; i++) {
+  meatList[i].src = "greenmeat.png";
+ }
+}
+
+//13. Use recycled paper products:
+function useRecycledPaper(){
+ var paperList = document.querySelectorAll("paper");
+ for(var i = 0; i < paperList.length; i++) {
+  paperList[i].src = "greenpaper.png";
+ }
+}
+
+//14. Recycle glass products:
+function recycleGlass(){
+ var glassList = document.querySelectorAll("glass");
+ for(var i = 0; i < glassList.length; i++) {
+  glassList[i].src = "greenglass.png";
+ }
+}
+
+//15. Use natural dyes and fabrics:
+function useNaturalDyes(){
+ var dyeList = document.querySelectorAll("dyenatural");
+ for(var i = 0; i < dyeList.length; i++) {
+  dyeList[i].src = "greendyenatural.png";
+ }
+}
+
+//16. Install natural insulation: 
+function installNaturalInsulation(){
+ var insulationList = document.querySelectorAll("insulation");
+ for(var i = 0; i < insulationList.length; i++) {
+  insulationList[i].src = "greeninsulation.png";
+ }
+}
+
+//17. Buy organic goods: 
+function buyOrganicGoods(){
+ var organicList = document.querySelectorAll("organic");
+ for(var i = 0; i < organicList.length; i++) {
+  organicList[i].src = "greenorganic.png";
+ }
+}
+
+//18. Eat less processed food:
+function eatLessProcessedFood(){
+ var processedList = document.querySelectorAll("processedfood");
+ for(var i = 0; i < processedList.length; i++) {
+  processedList[i].src = "greenprocessedfood.png";
+ }
+}
+
+//19. Reduce paper consumption:
+function reducePaperConsumption(){
+ var paperConsumptionList = document.querySelectorAll("paperconsumption");
+ for(var i = 0; i < paperConsumptionList.length; i++) {
+  paperConsumptionList[i].src = "greenpaperconsumption.png";
+ }
+}
+
+//20. Utilize renewable energy: 
+function utilizeRenewableEnergy(){
+ var renewableList = document.querySelectorAll("renewable");
+ for(var i = 0; i < renewableList.length; i++) {
+  renewableList[i].src = "greenrenewable.png";
+ }
+}
+
+//21. Reuse materials: 
+function reuseMaterials(){
+ var materialsList = document.querySelectorAll("materials");
+ for(var i = 0; i < materialsList.length; i++) {
+  materialsList[i].src = "greenmaterials.png";
+ }
+}
+
+//22. Get rid of all plastic bags:
+function getRidOfPlasticBags(){
+ var plasticList = document.querySelectorAll("plasticbag");
+ for(var i = 0; i < plasticList.length; i++) {
+  plasticList[i].src = "greenplasticbag.png";
+ }
+}
+
+//23. Plant flowers and shrubs:
+function plantFlowers(){
+ var flowerList = document.querySelectorAll("flower");
+ for(var i = 0; i < flowerList.length; i++) {
+  flowerList[i].src = "greenflower.png";
+ }
+}
+
+//24. Create a compost bin: 
+function createCompost(){
+ var compostList = document.querySelectorAll("compost");
+ for(var i = 0; i < compostList.length; i++) {
+  compostList[i].src = "greencompost.png";
+ }
+}
+
+//25. Take shorter showers:
+function takeShorterShowers(){
+ var showerList = document.querySelectorAll("shower");
+ for(var i = 0; i < showerList.length; i++) {
+  showerList[i].src = "greenshower.png";
+ }
+}
+
+//26. Utilize natural daylight:
+function utilizeDaylight(){
+ var daylightList = document.querySelectorAll("daylight");
+ for(var i = 0; i < daylightList.length; i++) {
+  daylightList[i].src = "greendaylight.png";
+ }
+}
+
+//27. Collect rainwater:
+function collectRainwater(){
+ var rainwaterList = document.querySelectorAll("rainwater");
+ for(var i = 0; i < rainwaterList.length; i++) {
+  rainwaterList[i].src = "greenrainwater.png";
+ }
+}
+
+//28. Buy energy-efficient appliances:
+function buyEnergyEfficientAppliances(){
+ var efficientList = document.querySelectorAll("efficientappliance");
+ for(var i = 0; i < efficientList.length; i++) {
+  efficientList[i].src = "greenefficientappliance.png";
+ }
+}
+
+//29. Use electric or hybrid cars:
+function useHybridCars(){
+ var hybridList = document.querySelectorAll("hybridcar");
+ for(var i = 0; i < hybridList.length; i++) {
+  hybridList[i].src = "greenhybridcar.png";
+ }
+}
+
+//30. Recycle paper, plastic, and glass:
+function recyclePaperPlasticGlass(){
+ var recycleList = document.querySelectorAll("recycle");
+ for(var i = 0; i < recycleList.length; i++) {
+  recycleList[i].src = "greenrecycle.png";
+ }
+}
+
+//31. Invest in renewable energy:
+function investInRenewableEnergy(){
+ var investList = document.querySelectorAll("investrenewable");
+ for(var i = 0; i < investList.length; i++) {
+ investList[i].src = "greeninvestrenewable.png";
+ }
+}
+
+//32. Stop using chemical pesticides:
+function stopUsingPesticides(){
+ var pesticideList = document.querySelectorAll("pesticides");
+ for(var i = 0; i < pesticideList.length; i++) {
+ pesticideList[i].src = "greenpesticides.png";
+ }
+}
+
+//33. Seek certification for green products:
+function seekGreenCertification(){
+ var certificationList = document.querySelectorAll("certification");
+ for(var i = 0; i < certificationList.length; i++) {
+ certificationList[i].src = "greencertification.png";
+ }
+}
+
+//34. Use green cleaning products:
+function useGreenCleaningProducts(){
+ var cleaningList = document.querySelectorAll("greencleaning");
+ for(var i = 0; i < cleaningList.length; i++) {
+  cleaningList[i].src = "greengreencleaning.png";
+ }
+}
+
+//35. Donate money to green causes:
+function donateMoneyToGreenCauses(){
+ var donationList = document.querySelectorAll("donation");
+ for(var i = 0; i < donationList.length; i++) {
+ donationList[i].src = "greendonation.png";
+ }
+}
+
+//36. Plant a garden: 
+function plantAGarden(){
+ var gardenList = document.querySelectorAll("garden");
+ for(var i = 0; i < gardenList.length; i++) {
+  gardenList[i].src = "greengarden.png";
+ }
+}
+
+//37. Use energy saving settings on electronic devices:
+function useEnergySavingSettings(){
+ var energySavingList = document.querySelectorAll("energysaving");
+ for(var i = 0; i < energySavingList.length; i++) {
+ energySavingList[i].src = "greenenergysaving.png";
+ }
+}
+
+//38. Buy locally grown food:
+function buyLocallyGrownFood(){
+ var foodList = document.querySelectorAll("localfood");
+ for(var i = 0; i < foodList.length; i++) {
+  foodList[i].src = "greenlocalfood.png";
+ }
+}
+
+//39. Collect rainwater for your garden and other use:
+function collectRainwaterForGarden(){
+ var rainList = document.querySelectorAll("raingarden");
+ for(var i = 0; i < rainList.length; i++) {
+  rainList[i].src = "greenraingarden.png";
+ }
+}
+
+//40. Use fans instead of air conditioner:
+function useFansInsteadOfAC(){
+ var fansList = document.querySelectorAll("fan");
+ for(var i = 0; i < fansList.length; i++) {
+  fansList[i].src = "greenfan.png";
+ }
+}
+
+//41. Use natural fertilizers in the garden:
+function useNaturalFertilizers(){
+ var fertilizerList = document.querySelectorAll("fertilizer");
+ for(var i = 0; i < fertilizerList.length; i++) {
+  fertilizerList[i].src = "greenfertilizer.png";
+ }
+}
+
+//42. Create a green team:
+function createGreenTeam(){
+ var greenTeamList = document.querySelectorAll("greenteam");
+ for(var i = 0; i < greenTeamList.length; i++) {
+  greenTeamList[i].src = "greengreenteam.png";
+ }
+}
+
+//43. Use natural air fresheners:
+function useNaturalAirFresheners(){
+ var airFreshenersList = document.querySelectorAll("airfreshener");
+ for(var i = 0; i < airFreshenersList.length; i++) {
+  airFreshenersList[i].src = "greenairfreshener.png";
+ }
+}
+
+//44. Use solar energy:
+function useSolarEnergy(){
+ var solarList = document.querySelectorAll("solarenergy");
+ for(var i = 0; i < solarList.length; i++) {
+  solarList[i].src = "greensolarenergy.png";
+ }
+}
+
+//45. Reduce your reliance on single-use plastics:
+function reduceSingleUsePlastics(){
+ var reduceList = document.querySelectorAll("reducesingleuse");
+ for(var i = 0; i < reduceList.length; i++) {
+ reduceList[i].src = "greenreducesingleuse.png";
+ }
+}
+
+//46. Buy second-hand items:
+function buySecondHandItems(){
+ var secondHandList = document.querySelectorAll("secondhand");
+ for(var i = 0; i < secondHandList.length; i++) {
+ secondHandList[i].src = "greensecondhand.png";
+ }
+}
+
+//47. Choose eco-friendly materials:
+function chooseEcoFriendly(){
+ var ecoList = document.querySelectorAll("ecofriendly");
+ for(var i = 0; i < ecoList.length; i++) {
+  ecoList[i].src = "greenecofriendly.png";
+ }
+}
+
+//48. Take your own mug for coffee:
+function
